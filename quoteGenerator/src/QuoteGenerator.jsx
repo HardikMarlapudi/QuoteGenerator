33ab041302e0840a85e6986e45ed3f6241643d42
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-10-17 17:38:54 -0400

    fixed broekn API key

--- quoteGenerator/src/QuoteGenerator.jsx
+++ quoteGenerator/src/QuoteGenerator.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 function QuoteGenerator() {
@@ -6,40 +6,35 @@ function QuoteGenerator() {
    const [quote, setQuote] = useState('');
    const [author, setAuthor] = useState('');
    
-   const GenerateQuote = async () => {
-      axios.get('https://api.api-ninjas.com/v1/quotes', {
-         method: 'GET',
-         headers: {
-            'X-Api-Key': 'Dv2bJ2q6yW2Y2x1Z7mY2gA==b0X9Q4VwKZ3r7w5L',
-            'Content-Type': 'application/json'
-         }
-      })
-      .then((response) => {
-         setQuote(data[0].quote);
-         setAuthor(data[0].quote);
-         setGenerateQuote(response.data[Math.floor(Math.random() * response.data.length)]);
-      })
-      .catch((error) => {
-         console.error('Error fetching quotes:', error);
-      });
+   const generateQuote = async () => {
+      try {
+         const response = await axios.get('https://api.api-ninjas.com/v1/quotes', {
+            headers: {
+               'X-Api-Key': '1QaEXXiMr42UQHgffVLNDA==uoRjZZaDnhJg5v1O',
+               'Content-Type': 'application/json'
+            }
+         });
+         const quoteData = response.data[0];
+         setQuote(quoteData.quote);
+         setAuthor(quoteData.author);
+      } catch (error) {
+         console.error("Error cannot fetch quote: ", error);
+         return;
+      }
    }
 
-  useEffect(() => {
-   GenerateQuote();
-  }, [])
-
    return (
-   <>
-    <h1 id="title">Quote Generator</h1>
+      <>
+      <h1 id="title">Quote Generator</h1>
 
-    <div className="border">
-      <p id="quote">{quote}</p>
-      <p id="author">{author}</p>
-    </div>
+      <div className="border">
+         <p id="quote">"{quote}"</p>
+         <p id="author">"{author}"</p>
+      </div>
 
-     <center><button id="getQuote" onClick={GenerateQuote}>GenerateQuote</button></center> 
-    </>
-   )
+      <center><button id="getQuote" onClick={generateQuote}>GenerateQuote</button></center> 
+      </>
+      )
 }
 
 export default QuoteGenerator;
